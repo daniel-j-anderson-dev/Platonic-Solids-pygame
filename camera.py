d4e7b352961cecf9ce98e3fb2ae9608688045ff3
Jacob Anderson <jacob.anderson.dev@gmail.com>
2023-01-08 17:50:02 -0700

    Changed some styling and naming conventions

--- camera.py
+++ camera.py
@@ -1,24 +1,35 @@
 import numpy as np
 import math
+
+
 class Camera:
     def __init__(self):
-        self.position     = [0,0,-150]  # The camera is on the negative z-axis
-        self.direction    = [0,0,1]     # The camera is pointing towards the positive z-axis
-        self.fovy         = 45          # Y angle Field of view in degrees
-        self.aspect_ratio = 1920/1080   # HD aspect ratio
-        self.dist_near    = 150         # Distance from camera to near clipping plane
-        self.dist_far     = 250         # Distance from camera to near clipping plane
+        
+        self.position = [0, 0, -150]                                # The camera is on the negative z-axis
+
+        # The camera is pointing towards the positive z-axis
+        self.direction = [0, 0, 1]
+        self.fovy = 45                                              # Y angle Field of view in degrees
+        self.aspect_ratio = 1920/1080                               # HD aspect ratio
+        self.dist_near = 150                                        # Distance from camera to near clipping plane
+        self.dist_far = 250                                         # Distance from camera to near clipping plane
         self.getProjectionMatrix()
 
     # i need to convert these coords to the camera coord system
     def getProjectionMatrix(self):
-        fovy = math.radians(self.fovy)                          # fovy to radians
-        n, f = self.dist_near, self.dist_far                    # near/far planes
-        t, b = n * math.tan(fovy / 2), -n * math.tan(fovy / 2)  # top/bottom planes
-        r, l = t * self.aspect_ratio, b * self.aspect_ratio     # right/left planes
-        projMat = np.array([[2*n/(r-l), 0,          (r+l)/(r-l),  0],
-                            [0,         2*n/(t-b),  (t+b)/(t-b),  0],
-                            [0,         0,         -(f+n)/(f-n), -2*f*n/(f-n)],
-                            [0,         0,         -1,            0]])
-        return projMat
 
+        # fovy to radians
+        fovy = math.radians(self.fovy)
+
+        n, f = self.dist_near, self.dist_far                        # near/far planes
+        t, b = n * math.tan(fovy / 2), -n * math.tan(fovy / 2)      # top/bottom planes
+        r, l = t * self.aspect_ratio, b * self.aspect_ratio         # right/left planes
+
+        projection_matrix = np.array([
+            [2*n/(r-l), 0,          (r+l)/(r-l),  0],
+            [0,         2*n/(t-b),  (t+b)/(t-b),  0],
+            [0,         0,         -(f+n)/(f-n), -2*f*n/(f-n)],
+            [0,         0,         -1,            0]
+        ])
+
+        return projection_matrix
