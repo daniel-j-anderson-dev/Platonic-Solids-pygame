4d5759e8fd1bb7269dec0605bbf335fbf7499ca3
Jacob Anderson <jacob.anderson.dev@gmail.com>
2023-01-08 17:59:59 -0700

    changed styling to meet python standards

--- camera.py
+++ camera.py
@@ -4,23 +4,32 @@ import math
 
 class Camera:
     def __init__(self):
-        
-        self.position = [0, 0, -150]                                # The camera is on the negative z-axis
-        self.direction = [0, 0, 1]                                  # The camera is pointing towards the positive z-axis
-        self.fovy = 45                                              # Y angle Field of view in degrees
+
+        # The camera is on the negative z-axis
+        self.position = [0, 0, -150]
+        # The camera is pointing towards the positive z-axis
+        self.direction = [0, 0, 1]
+        # Y angle Field of view in degrees
+        self.fovy = 45
         self.aspect_ratio = 1920/1080                               # HD aspect ratio
-        self.dist_near = 150                                        # Distance from camera to near clipping plane
-        self.dist_far = 250                                         # Distance from camera to near clipping plane
+        # Distance from camera to near clipping plane
+        self.dist_near = 150
+        # Distance from camera to near clipping plane
+        self.dist_far = 250
         self.getProjectionMatrix()
 
     # i need to convert these coords to the camera coord system
     def getProjectionMatrix(self):
 
-        fovy = math.radians(self.fovy)                              # fovy to radians
+        # fovy to radians
+        fovy = math.radians(self.fovy)
 
-        n, f = self.dist_near, self.dist_far                        # near/far planes distances from camera
-        t, b = n * math.tan(fovy / 2), -n * math.tan(fovy / 2)      # top/bottom planes distances from camera
-        r, l = t * self.aspect_ratio, b * self.aspect_ratio         # right/left planes distances from camera
+        # near/far planes distances from camera
+        n, f = self.dist_near, self.dist_far
+        # top/bottom planes distances from camera
+        t, b = n * math.tan(fovy / 2), -n * math.tan(fovy / 2)
+        # right/left planes distances from camera
+        r, l = t * self.aspect_ratio, b * self.aspect_ratio
 
         projection_matrix = np.array([
             [2*n/(r-l), 0,          (r+l)/(r-l),  0],
--- main.py
+++ main.py
@@ -2,27 +2,26 @@ import pygame as pg
 import shape3d
 import camera
 
+
 class Renderer:
 
     def __init__(self):
 
         pg.init()
-        self.WINDOW_SIZE    = (1920, 1080)
-        self.ORIGIN         = [self.WINDOW_SIZE[0]//2, self.WINDOW_SIZE[1]//2, 0]
-        self.FPS            = 60
-        self.screen         = pg.display.set_mode(self.WINDOW_SIZE)
-        self.clock          = pg.time.Clock()
-        self.shapes         = []
-        self.keys           = pg.key.get_pressed()
-        self.speed          = 5
-        self.delta_theta    = 5
-        self.camera         = camera.Camera()
-
+        self.WINDOW_SIZE = (1920, 1080)
+        self.ORIGIN = [self.WINDOW_SIZE[0]//2, self.WINDOW_SIZE[1]//2, 0]
+        self.FPS = 60
+        self.screen = pg.display.set_mode(self.WINDOW_SIZE)
+        self.clock = pg.time.Clock()
+        self.shapes = []
+        self.keys = pg.key.get_pressed()
+        self.speed = 5
+        self.delta_theta = 5
+        self.camera = camera.Camera()
 
     def Platonic_Solids(self):
 
-        return [shape3d.Cube([-700, -300, 0]), shape3d.Tetrahedron([-700, 300, 0]), shape3d.Dodecahedron([0,0,0]), shape3d.Icosahedron([700,-300,0]), shape3d.Octahedron([700, 300, 0])]
-
+        return [shape3d.Cube([-700, -300, 0]), shape3d.Tetrahedron([-700, 300, 0]), shape3d.Dodecahedron([0, 0, 0]), shape3d.Icosahedron([700, -300, 0]), shape3d.Octahedron([700, 300, 0])]
 
     def DrawPerspective(self):
 
@@ -30,45 +29,60 @@ class Renderer:
         for shape in self.shapes:
             shape.DrawPerspective(self.screen, self.camera, self.ORIGIN)
 
-
     def Draw(self):
 
         self.screen.fill(pg.Color('white'))
         for shape in self.shapes:
             shape.Draw(self.screen, self.ORIGIN)
 
-
     def Input(self):
 
-        if self.keys[pg.K_i]: self.camera.position[0] += self.speed
-        if self.keys[pg.K_k]: self.camera.position[0] -= self.speed
-        if self.keys[pg.K_j]: self.camera.position[1] += self.speed
-        if self.keys[pg.K_l]: self.camera.position[1] -= self.speed
-        if self.keys[pg.K_0]: self.shapes = self.Platonic_Solids()                    # reset angles and pos
-
-        for shape in self.shapes:
-            
-            if self.keys[pg.K_LEFT]:     shape.position[0]  += -self.speed            #TRANSLATE
-            if self.keys[pg.K_RIGHT]:    shape.position[0]  +=  self.speed
-            if self.keys[pg.K_DOWN]:     shape.position[1]  +=  self.speed
-            if self.keys[pg.K_UP]:       shape.position[1]  += -self.speed
-            if self.keys[pg.K_PAGEUP]:   shape.position[2]  +=  self.speed
-            if self.keys[pg.K_PAGEDOWN]: shape.position[2]  += -self.speed
-            if self.keys[pg.K_s]:        shape.Rotate((1,0,0), -self.delta_theta)      #ROTATE
-            if self.keys[pg.K_w]:        shape.Rotate((1,0,0),  self.delta_theta)
-            if self.keys[pg.K_d]:        shape.Rotate((0,1,0), -self.delta_theta)
-            if self.keys[pg.K_a]:        shape.Rotate((0,1,0),  self.delta_theta)
-            if self.keys[pg.K_e]:        shape.Rotate((0,0,1), -self.delta_theta)
-            if self.keys[pg.K_q]:        shape.Rotate((0,0,1),  self.delta_theta)
-            if self.keys[pg.K_SPACE]:    self.Rotate()
-    
-
-    def Rotate(self):                   #rotates shapes along (1/sqrt(3), 1/sqrt(3), 1/sqrt(3)) = (1,1,1)/|(1,1,1)|
+        if self.keys[pg.K_i]:
+            self.camera.position[0] += self.speed
+        if self.keys[pg.K_k]:
+            self.camera.position[0] -= self.speed
+        if self.keys[pg.K_j]:
+            self.camera.position[1] += self.speed
+        if self.keys[pg.K_l]:
+            self.camera.position[1] -= self.speed
+        if self.keys[pg.K_0]:
+            self.shapes = self.Platonic_Solids()                    # reset angles and pos
 
         for shape in self.shapes:
 
-            shape.Rotate((0.57735026919 ,0.57735026919 ,0.57735026919 ), self.delta_theta/13)
+            if self.keys[pg.K_LEFT]:
+                shape.position[0] += -self.speed  # TRANSLATE
+            if self.keys[pg.K_RIGHT]:
+                shape.position[0] += self.speed
+            if self.keys[pg.K_DOWN]:
+                shape.position[1] += self.speed
+            if self.keys[pg.K_UP]:
+                shape.position[1] += -self.speed
+            if self.keys[pg.K_PAGEUP]:
+                shape.position[2] += self.speed
+            if self.keys[pg.K_PAGEDOWN]:
+                shape.position[2] += -self.speed
+            if self.keys[pg.K_s]:
+                shape.Rotate((1, 0, 0), -self.delta_theta)  # ROTATE
+            if self.keys[pg.K_w]:
+                shape.Rotate((1, 0, 0),  self.delta_theta)
+            if self.keys[pg.K_d]:
+                shape.Rotate((0, 1, 0), -self.delta_theta)
+            if self.keys[pg.K_a]:
+                shape.Rotate((0, 1, 0),  self.delta_theta)
+            if self.keys[pg.K_e]:
+                shape.Rotate((0, 0, 1), -self.delta_theta)
+            if self.keys[pg.K_q]:
+                shape.Rotate((0, 0, 1),  self.delta_theta)
+            if self.keys[pg.K_SPACE]:
+                self.Rotate()
 
+    def Rotate(self):  # rotates shapes along (1/sqrt(3), 1/sqrt(3), 1/sqrt(3)) = (1,1,1)/|(1,1,1)|
+
+        for shape in self.shapes:
+
+            shape.Rotate((0.57735026919, 0.57735026919,
+                         0.57735026919), self.delta_theta/13)
 
     def Handle_Events(self):
 
@@ -78,13 +92,11 @@ class Renderer:
                 pg.quit()
                 exit()
 
-
     def Update(self):
 
         pg.display.set_caption(str(self.clock.get_fps()))
         pg.display.flip()
         self.clock.tick(self.FPS)
-
 
     def Run(self):
 
@@ -107,9 +119,9 @@ class Renderer:
 
             self.Update()
             self.Handle_Events()
-                
+
 
 if __name__ == "__main__":
-  program = Renderer()                          # init render
-  program.shapes = program.Platonic_Solids()    # populate objects to be rendered
-  program.Run()                                 # Render objects
+    program = Renderer()                          # init render
+    program.shapes = program.Platonic_Solids()    # populate objects to be rendered
+    program.Run()                                 # Render objects
