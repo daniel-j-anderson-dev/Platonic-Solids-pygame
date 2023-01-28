817d840dd689088a58397d1debe520d8d1a546b9
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-01-28 01:55:20 -0500

    changed design style to MVC

--- Shape3D.py
+++ Shape3D.py
@@ -13,57 +13,13 @@ class Shape3D:
         self.position = position
         self.name = name
 
-    def project_point(self, camera, point):
-        p = np.array([point[0], point[1], point[2], 1])
-        projected_point = np.matmul(camera.getProjectionMatrix(), p)
-        # un homogenize projected point
-        return list([projected_point[0], projected_point[1], projected_point[2]])
-
-    def Project(self, camera):
-        projected_vertices = []
-        for vertex in self.vertices:
-            projected_vertices.append(self.project_point(camera, vertex))
-        return projected_vertices
-
-    # uses quaternion to rotate a + bi + cj + dk
-    def rotate_point(self, point, axis, angle):
-        angle = math.radians(angle)
-        x, y, z = point[0], point[1], point[2]
-        a, b, c, d = 1 * math.cos(angle/2), axis[0] * math.sin(
-            angle/2), axis[1] * math.sin(angle/2), axis[2] * math.sin(angle/2)
-        return [(x*(1 - 2*c**2 - 2*d**2) + y*(2*b*c - 2*d*a) + z*(2*b*d + 2*c*a)),
-                (x*(2*b * c + 2*d*a) + y*(1 - 2*b**2 - 2*d**2) + z*(2*c*d - 2*b*a)),
-                (x*(2*b * d - 2*c*a) + y*(2*c*d + 2*b*a) + z*(1 - 2*b**2 - 2*c**2))]
-
-    def Rotate(self, axis, angle):
-        rotated_vertices = []
-        for vertex in self.vertices:
-            rotated_vertices.append(self.rotate_point(vertex, axis, angle))
-        self.vertices = rotated_vertices
-
-    def DrawPerspective(self, screen, camera, ORIGIN):
-        projVerts = self.Project(camera)
-        for edge in self.edges:
-            start_point = ((self.position[0] + projVerts[edge[0]][0] + ORIGIN[0]),
-                           (self.position[1] - projVerts[edge[0]][1] + ORIGIN[1]))
-            end_point = ((self.position[0] + projVerts[edge[1]][0] + ORIGIN[0]),
-                         (self.position[1] - projVerts[edge[1]][1] + ORIGIN[1]))
-            pg.draw.line(screen, [0, 0, 0], start_point, end_point, 1)
-
-    def Draw(self, screen, ORIGIN):
-        for edge in self.edges:
-            start_point = ((self.position[0] + self.vertices[edge[0]][0] + ORIGIN[0]),
-                           (self.position[1] - self.vertices[edge[0]][1] + ORIGIN[1]))
-            end_point = ((self.position[0] + self.vertices[edge[1]][0] + ORIGIN[0]),
-                         (self.position[1] - self.vertices[edge[1]][1] + ORIGIN[1]))
-            pg.draw.line(screen, [0, 0, 0], start_point, end_point, 1)
-
-
-class Tetrahedron(Shape3D,):
+class Tetrahedron(Shape3D):
     def __init__(self, position):
         vertices = [[SCALE, SCALE, SCALE], [-SCALE, -SCALE, SCALE],
                     [-SCALE, SCALE, -SCALE], [SCALE, -SCALE, -SCALE]]
         edges = [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]]
+        # for vertex in vertices:
+#             vertex[2] += SCALE
         super().__init__(vertices, edges, "Tetrahedron", position)
 
 
@@ -73,6 +29,8 @@ class Cube(Shape3D):
                     [-SCALE, SCALE, SCALE], [-SCALE, SCALE, -SCALE], [-SCALE, -SCALE, SCALE], [-SCALE, -SCALE, -SCALE]]
         edges = [[0, 1], [0, 2], [0, 4], [1, 3], [1, 5], [2, 3],
                  [2, 6], [3, 7], [4, 5], [4, 6], [5, 7], [6, 7]]
+        # for vertex in vertices:
+#             vertex[2] += SCALE
         super().__init__(vertices, edges, "Cube", position)
 
 
@@ -86,19 +44,26 @@ class Square(Shape3D):
 
 class Dodecahedron(Shape3D):
     def __init__(self, position):
-        vertices = [[SCALE, SCALE, SCALE], [SCALE, SCALE, -SCALE], [SCALE, -SCALE, SCALE], [SCALE, -SCALE, -SCALE], [-SCALE, SCALE, SCALE], [-SCALE, SCALE, -SCALE], [-SCALE, -SCALE, SCALE], [-SCALE, -SCALE, -SCALE], [0, SCALE/PHI, SCALE*PHI], [0, SCALE/PHI, -SCALE*PHI], [0, -SCALE /
-                                                                                                                                                                                                                                                                                PHI, SCALE*PHI], [0, -SCALE/PHI, -SCALE*PHI], [SCALE/PHI, SCALE*PHI, 0], [SCALE/PHI, -SCALE*PHI, 0], [-SCALE/PHI, SCALE*PHI, 0], [-SCALE/PHI, -SCALE*PHI, 0], [SCALE*PHI, 0, SCALE/PHI], [SCALE*PHI, 0, -SCALE/PHI], [-SCALE*PHI, 0, SCALE/PHI], [-SCALE*PHI, 0, -SCALE/PHI]]
+        vertices = [[SCALE, SCALE, SCALE], [SCALE, SCALE, -SCALE], [SCALE, -SCALE, SCALE], [SCALE, -SCALE, -SCALE],
+                    [-SCALE, SCALE, SCALE], [-SCALE, SCALE, -SCALE], [-SCALE, -SCALE, SCALE], [-SCALE, -SCALE, -SCALE],
+                    [0, SCALE/PHI, SCALE*PHI], [0, SCALE/PHI, -SCALE*PHI], [0, -SCALE /PHI, SCALE*PHI], [0, -SCALE/PHI, -SCALE*PHI],
+                    [SCALE/PHI, SCALE*PHI, 0], [SCALE/PHI, -SCALE*PHI, 0], [-SCALE/PHI, SCALE*PHI, 0], [-SCALE/PHI, -SCALE*PHI, 0],
+                    [SCALE*PHI, 0, SCALE/PHI], [SCALE*PHI, 0, -SCALE/PHI], [-SCALE*PHI, 0, SCALE/PHI], [-SCALE*PHI, 0, -SCALE/PHI]]
         edges = [[0, 8], [0, 12], [0, 16], [1, 9], [1, 12], [1, 17], [2, 10], [2, 13], [2, 16], [3, 11], [3, 13], [3, 17], [4, 8], [4, 14], [4, 18], [
-            5, 9], [5, 14], [5, 19], [6, 10], [6, 15], [6, 18], [7, 11], [7, 15], [7, 19], [8, 10], [9, 11], [12, 14], [13, 15], [16, 17], [18, 19], ]
+            5, 9], [5, 14], [5, 19], [6, 10], [6, 15], [6, 18], [7, 11], [7, 15], [7, 19], [8, 10], [9, 11], [12, 14], [13, 15], [16, 17], [18, 19]]
+        # for vertex in vertices:
+#             vertex[2] += SCALE
         super().__init__(vertices, edges, "Dodecahedron", position)
 
 
 class Icosahedron(Shape3D):
     def __init__(self, position):
         vertices = [[0, SCALE, SCALE*PHI], [0, SCALE, -SCALE*PHI], [0, -SCALE, SCALE*PHI], [0, -SCALE, -SCALE*PHI], [SCALE, SCALE*PHI, 0], [SCALE, -SCALE*PHI, 0],
-                    [-SCALE, SCALE*PHI, 0], [-SCALE, -SCALE*PHI, 0], [SCALE*PHI, 0, SCALE], [SCALE*PHI, 0, -SCALE], [-SCALE*PHI, 0, SCALE], [-SCALE*PHI, 0, -SCALE], ]
+                    [-SCALE, SCALE*PHI, 0], [-SCALE, -SCALE*PHI, 0], [SCALE*PHI, 0, SCALE], [SCALE*PHI, 0, -SCALE], [-SCALE*PHI, 0, SCALE], [-SCALE*PHI, 0, -SCALE]]
         edges = [[0, 2], [0, 4], [0, 6], [0, 8], [0, 10], [1, 3], [1, 4], [1, 6], [1, 9], [1, 11], [2, 5], [2, 7], [2, 8], [2, 10], [3, 5], [
             3, 7], [3, 9], [3, 11], [4, 6], [4, 8], [4, 9], [5, 7], [5, 8], [5, 9], [6, 10], [6, 11],  [7, 10], [7, 11], [8, 9], [10, 11]]
+        # for vertex in vertices:
+#             vertex[2] += SCALE
         super().__init__(vertices, edges, "Icosahedron", position)
 
 
@@ -108,4 +73,6 @@ class Octahedron(Shape3D):
                     [0, -SCALE, 0], [0, 0, SCALE], [0, 0, -SCALE]]
         edges = [[0, 2], [0, 3], [0, 4], [0, 5], [1, 2], [1, 3],
                  [1, 4], [1, 5], [2, 4], [2, 5], [3, 4], [3, 5]]
+        # for vertex in vertices:
+#             vertex[2] += SCALE
         super().__init__(vertices, edges, "Octahedron", position)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import math
 import shape3d
 import camera
 
@@ -23,17 +24,69 @@ class Renderer:
 
         return [shape3d.Cube([-700, -300, 0]), shape3d.Tetrahedron([-700, 300, 0]), shape3d.Dodecahedron([0, 0, 0]), shape3d.Icosahedron([700, -300, 0]), shape3d.Octahedron([700, 300, 0])]
 
-    def DrawPerspective(self):
+    def rotate_point(self, point, axis, angle):
+
+        angle = math.radians(angle)
+        x, y, z = point[0], point[1], point[2]
+        a, b, c, d = 1 * math.cos(angle/2), axis[0] * math.sin(
+            angle/2), axis[1] * math.sin(angle/2), axis[2] * math.sin(angle/2)
+        norm = math.sqrt(a*a + b*b + c*c + d*d)
+        a /= norm
+        b /= norm
+        c /= norm
+        d /= norm
+        rotatedpoint = [(x*(1 - 2*c**2 - 2*d**2) + y*(2*b*c - 2*d*a) + z*(2*b*d + 2*c*a)),
+                        (x*(2*b * c + 2*d*a) + y*(1 - 2*b**2 - 2*d**2) + z*(2*c*d - 2*b*a)),
+                        (x*(2*b * d - 2*c*a) + y*(2*c*d + 2*b*a) + z*(1 - 2*b**2 - 2*c**2))]
+        return rotatedpoint
+
+    def rotate_point_about_another(self, point, centerOfRotation, axis, angle):
+
+        angle = math.radians(angle)
+        x, y, z = point[0] - centerOfRotation[0], point[1] - centerOfRotation[1], point[2] - centerOfRotation[2]
+        a, b, c, d = 1 * math.cos(angle/2), axis[0] * math.sin(
+            angle/2), axis[1] * math.sin(angle/2), axis[2] * math.sin(angle/2)
+        norm = math.sqrt(a*a + b*b + c*c + d*d)
+        a /= norm
+        b /= norm
+        c /= norm
+        d /= norm
+        rotatedpoint = [(x*(1 - 2*c**2 - 2*d**2) + y*(2*b*c - 2*d*a) + z*(2*b*d + 2*c*a)) + centerOfRotation[0],
+                        (x*(2*b * c + 2*d*a) + y*(1 - 2*b**2 - 2*d**2) + z*(2*c*d - 2*b*a)) + centerOfRotation[1],
+                        (x*(2*b * d - 2*c*a) + y*(2*c*d + 2*b*a) + z*(1 - 2*b**2 - 2*c**2)) + centerOfRotation[2]]
+        return rotatedpoint
+
+    def RotateShapes(self, axis, angle):
+
+        for shape in self.shapes:
+            rotated_vertices = []
+            for vertex in shape.vertices:
+                rotated_vertices.append(self.rotate_point(vertex, axis, angle))
+            shape.vertices = rotated_vertices
+
+    def RotateShapesLocal(self, axis, angle):
+
+        for shape in self.shapes:
+            rotated_vertices = []
+            for vertex in shape.vertices:
+                rotated_vertices.append(self.rotate_point_about_another(vertex, shape.position, axis, angle))
+            shape.vertices = rotated_vertices
+
+    def TranslateShapes(self, speed, index):
+        for shape in self.shapes:
+            shape.position[index] += speed
+
+    def DrawShapes(self):
 
         self.screen.fill(pg.Color('white'))
         for shape in self.shapes:
-            shape.DrawPerspective(self.screen, self.camera, self.ORIGIN)
+            for edge in shape.edges:
+                start_point = ((shape.position[0] + shape.vertices[edge[0]][0] + self.ORIGIN[0]),
+                               (shape.position[1] - shape.vertices[edge[0]][1] + self.ORIGIN[1]))
+                end_point   = ((shape.position[0] + shape.vertices[edge[1]][0] + self.ORIGIN[0]),
+                               (shape.position[1] - shape.vertices[edge[1]][1] + self.ORIGIN[1]))
+                pg.draw.line(self.screen, [0, 0, 0], start_point, end_point, 1)
 
-    def Draw(self):
-
-        self.screen.fill(pg.Color('white'))
-        for shape in self.shapes:
-            shape.Draw(self.screen, self.ORIGIN)
 
     def Input(self):
 
@@ -48,46 +101,54 @@ class Renderer:
         if self.keys[pg.K_0]:
             self.shapes = self.Platonic_Solids()                    # reset angles and pos
 
-        for shape in self.shapes:
+        if self.keys[pg.K_LEFT]:
+            self.TranslateShapes(-self.speed, 0)  # TRANSLATE
+        if self.keys[pg.K_RIGHT]:
+            self.TranslateShapes(self.speed, 0)
+        if self.keys[pg.K_DOWN]:
+            self.TranslateShapes(self.speed, 1)
+        if self.keys[pg.K_UP]:
+            self.TranslateShapes(-self.speed, 1)
+        if self.keys[pg.K_PAGEUP]:
+            self.TranslateShapes(self.speed, 2)
+        if self.keys[pg.K_PAGEDOWN]:
+            self.TranslateShapes(-self.speed, 2)
 
-            if self.keys[pg.K_LEFT]:
-                shape.position[0] += -self.speed  # TRANSLATE
-            if self.keys[pg.K_RIGHT]:
-                shape.position[0] += self.speed
-            if self.keys[pg.K_DOWN]:
-                shape.position[1] += self.speed
-            if self.keys[pg.K_UP]:
-                shape.position[1] += -self.speed
-            if self.keys[pg.K_PAGEUP]:
-                shape.position[2] += self.speed
-            if self.keys[pg.K_PAGEDOWN]:
-                shape.position[2] += -self.speed
+        if not self.keys[pg.K_LSHIFT]:
             if self.keys[pg.K_s]:
-                shape.Rotate((1, 0, 0), -self.delta_theta)  # ROTATE
+                self.RotateShapes((1, 0, 0), -self.delta_theta)  # ROTATE
             if self.keys[pg.K_w]:
-                shape.Rotate((1, 0, 0),  self.delta_theta)
+                self.RotateShapes((1, 0, 0),  self.delta_theta)
             if self.keys[pg.K_d]:
-                shape.Rotate((0, 1, 0), -self.delta_theta)
+                self.RotateShapes((0, 1, 0), -self.delta_theta)
             if self.keys[pg.K_a]:
-                shape.Rotate((0, 1, 0),  self.delta_theta)
+                self.RotateShapes((0, 1, 0),  self.delta_theta)
             if self.keys[pg.K_e]:
-                shape.Rotate((0, 0, 1), -self.delta_theta)
+                self.RotateShapes((0, 0, 1), -self.delta_theta)
             if self.keys[pg.K_q]:
-                shape.Rotate((0, 0, 1),  self.delta_theta)
+                self.RotateShapes((0, 0, 1),  self.delta_theta)
             if self.keys[pg.K_SPACE]:
-                self.Rotate()
+                self.RotateShapes((1/math.sqrt(3), 1/math.sqrt(3), 1/math.sqrt(3)), self.delta_theta)
 
-    def Rotate(self):  # rotates shapes along (1/sqrt(3), 1/sqrt(3), 1/sqrt(3)) = (1,1,1)/|(1,1,1)|
-
-        for shape in self.shapes:
-
-            shape.Rotate((0.57735026919, 0.57735026919,
-                         0.57735026919), self.delta_theta/13)
+        else:
+            if self.keys[pg.K_s]:
+                self.RotateShapesLocal((1, 0, 0), -self.delta_theta)  # ROTATE
+            if self.keys[pg.K_w]:
+                self.RotateShapesLocal((1, 0, 0),  self.delta_theta)
+            if self.keys[pg.K_d]:
+                self.RotateShapesLocal((0, 1, 0), -self.delta_theta)
+            if self.keys[pg.K_a]:
+                self.RotateShapesLocal((0, 1, 0),  self.delta_theta)
+            if self.keys[pg.K_e]:
+                self.RotateShapesLocal((0, 0, 1), -self.delta_theta)
+            if self.keys[pg.K_q]:
+                self.RotateShapesLocal((0, 0, 1),  self.delta_theta)
+            if self.keys[pg.K_SPACE]:
+                self.RotateShapesLocal((1/math.sqrt(3), 1/math.sqrt(3), 1/math.sqrt(3)), self.delta_theta)
 
     def Handle_Events(self):
 
         for event in pg.event.get():
-
             if event.type == pg.QUIT or self.keys[pg.K_ESCAPE]:
                 pg.quit()
                 exit()
@@ -100,25 +161,11 @@ class Renderer:
 
     def Run(self):
 
-        is_orthographic = True
         while True:
 
             self.keys = pg.key.get_pressed()
-
-            for event in pg.event.get():
-                if event.type == pg.KEYDOWN:
-                    is_orthographic = not is_orthographic
-
-            """if self.keys[pg.K_RETURN]:
-                is_orthographic = not is_orthographic"""
-
             self.Input()
-
-            if is_orthographic:
-                self.Draw()
-            else:
-                self.DrawPerspective()
-
+            self.DrawShapes()
             self.Update()
             self.Handle_Events()
 
